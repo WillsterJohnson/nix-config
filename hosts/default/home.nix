b9bb7a788687fcd66b137007e519f8bcba5fcfe7
WillsterJohnson <willster@willsterjohnson.com>
2024-10-03 22:27:12 +0100

    97 current  2024-10-03 22:22:28  24.11.20240919.c04d565       6.6.52                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -13,21 +13,6 @@
     }) {
     };
 in {
-  # https://nix-community.github.io/home-manager/options.xhtml
-  imports = [
-    ../../modules/home-manager/bat.nix
-    ../../modules/home-manager/bun.nix
-    # ../../modules/home-manager/firefox.nix
-    ../../modules/home-manager/fzf.nix
-    ../../modules/home-manager/git.nix
-    ../../modules/home-manager/lsd.nix
-    ../../modules/home-manager/oh-my-posh.nix
-    ../../modules/home-manager/ssh.nix
-    ../../modules/home-manager/thefuck.nix
-    ../../modules/home-manager/zoxide.nix
-    ../../modules/home-manager/zsh.nix
-  ];
-
   nixpkgs.config.packageOverrides = pkgs: {
     nur =
       import (builtins.fetchTarball {
@@ -39,6 +24,20 @@ in {
         inherit pkgs;
       };
   };
+  # https://nix-community.github.io/home-manager/options.xhtml
+  imports = [
+    ../../modules/home-manager/bat.nix
+    ../../modules/home-manager/bun.nix
+    ../../modules/home-manager/firefox.nix
+    ../../modules/home-manager/fzf.nix
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/lsd.nix
+    ../../modules/home-manager/oh-my-posh.nix
+    ../../modules/home-manager/ssh.nix
+    ../../modules/home-manager/thefuck.nix
+    ../../modules/home-manager/zoxide.nix
+    ../../modules/home-manager/zsh.nix
+  ];
 
   home = {
     username = "willsterjohnson";
