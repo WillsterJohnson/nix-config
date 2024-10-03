f6cde66cc3746a8f706296684e57fd39ee083db6
WillsterJohnson <willster@willsterjohnson.com>
2024-10-03 22:21:30 +0100

    95 current  2024-10-03 22:21:25  24.11.20240919.c04d565       6.6.52                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -3,7 +3,16 @@
   config,
   lib,
   ...
-}: {
+}: let
+  nur-no-pkgs =
+    import (builtins.fetchTarball {
+      # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
+      url = "https://github.com/nix-community/NUR/archive/47d63dcaa78c3b29d0fecc96f07a0103d7a94f1d.tar.gz";
+      # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
+      sha256 = "1prwiwhj6fjwwmrdg82yzmr0j84vi8z0w2h59brn838ggyq25lfv";
+    }) {
+    };
+in {
   # https://nix-community.github.io/home-manager/options.xhtml
   imports = [
     ../../modules/home-manager/bat.nix
