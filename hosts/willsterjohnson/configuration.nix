7eb463bc590f560fc598f82a75cac5374c9908df
WillsterJohnson <willster@willsterjohnson.com>
2024-10-05 14:31:13 +0100

    146 current  2024-10-05 14:31:06  24.11.20240919.c04d565       6.6.52                          *

--- hosts/willsterjohnson/configuration.nix
+++ hosts/willsterjohnson/configuration.nix
@@ -57,6 +57,16 @@
   networking.networkmanager.enable = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
+  services.fwupd.enable = true;
+  # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+  services.fwupd.package =
+    (import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+        sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+      }) {
+        inherit (pkgs) system;
+      })
+    .fwupd;
   services.gnome.gnome-browser-connector.enable = true;
   services.printing.enable = true;
   services.pipewire = {
