8743fb0086cf2cb338d2c85fe995d6cc26336898
WillsterJohnson <willster@willsterjohnson.com>
2024-10-06 11:11:44 +0100

    156 current  2024-10-06 11:11:36  24.11.20240919.c04d565       6.6.52                          *

--- hosts/willsterjohnson/.homefiles/buni/buni.sh
+++ hosts/willsterjohnson/.homefiles/buni/buni.sh
@@ -1,5 +1,5 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 function buni() {
-	SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 	echo $SCRIPT_DIR
 	exit
 	BUN_SUBCOMMAND="i"
