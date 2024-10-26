9ebc8a854555b98b86b966fbaf540de13c54c072
WillsterJohnson <willster@willsterjohnson.com>
2024-10-26 21:46:14 +0100

    210 current  2024-10-26 21:18:01  24.11.20241018.ccc0c21  6.6.57                          *

--- hosts/willsterjohnson/programs/bat.nix
+++ hosts/willsterjohnson/programs/bat.nix
@@ -2,16 +2,16 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "catppuccinMocha";
+      # theme = "catppuccinMocha";
     };
-    themes.catppuccinMocha = {
-      src = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "bat";
-        rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
-        hash = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
-      };
-      file = "themes/Catppuccin Mocha.tmTheme";
-    };
+    # themes.catppuccinMocha = {
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "catppuccin";
+    #     repo = "bat";
+    #     rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
+    #     hash = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
+    #   };
+    #   file = "themes/Catppuccin Mocha.tmTheme";
+    # };
   };
 }
