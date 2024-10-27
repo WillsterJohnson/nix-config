d0d0d6581c0ade2f418ef7263ba2b2b41238943f
WillsterJohnson <willster@willsterjohnson.com>
2024-10-27 13:37:07 +0000

    233 current  2024-10-27 13:36:59  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/programs/stylix.nix
+++ hosts/willsterjohnson/programs/stylix.nix
@@ -5,7 +5,7 @@
     image = ./stylix.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     cursor = {
-      name = "Catppuccin-mocha-lavender";
+      name = "catppuccin-mocha-lavender";
       package = pkgs.catppuccin-cursors.mochaLavender;
       size = 36;
     };
