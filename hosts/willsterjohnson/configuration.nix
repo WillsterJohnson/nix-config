a463d8f3e540808f09a25926e5d85caab4e67a96
WillsterJohnson <willster@willsterjohnson.com>
2024-11-10 11:53:58 +0000

    248 current  2024-11-10 11:30:13  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/configuration.nix
+++ hosts/willsterjohnson/configuration.nix
@@ -1,9 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
     ./willsterjohnson.nix
-    ./programs/stylix.nix
   ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -57,6 +56,51 @@
     variant = "";
   };
   services.openssh.enable = true;
+  # todo: cursors don't work correctly - on rebuild the cursor icons are not set correctly
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    image = ./desktop.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    cursor = {
+      name = "Catppuccin-mocha-lavender";
+      package = pkgs.catppuccin-cursors.mochaLavender;
+      size = 36;
+    };
+    fonts = {
+      monospace = {
+        package = with pkgs; (nerdfonts.override {fonts = ["Meslo"];});
+        name = "Meslo Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sizes = {
+        applications = 11;
+        terminal = 12;
+        desktop = 11;
+        popups = 11;
+      };
+    };
+    opacity = {
+      applications = 1.0;
+      terminal = 0.95;
+      desktop = 1.0;
+      popups = 1.0;
+    };
+    polarity = "dark";
+    targets = {
+      grub.enable = false;
+      gnome.enable = false;
+      gtk.enable = true;
+      nixos-icons.enable = true;
+    };
+  };
   system.stateVersion = "24.05";
   time.timeZone = "Europe/London";
 }
--- hosts/willsterjohnson/home.nix
+++ hosts/willsterjohnson/home.nix
@@ -1,27 +1,23 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: let
+  # https://github.com/nix-community/home-manager/issues/3447#issuecomment-2213029759
   autostartFile = builtins.listToAttrs (map
     (pkg: {
-      name = ".config/autostart/" + pkg.pname + ".desktop";
+      name = ".config/autostart/${pkg.pname}.desktop";
       value =
         if pkg ? desktopItem
         then {text = pkg.desktopItem.text;}
         else {
           source = with builtins; let
             appsPath = "${pkg}/share/applications";
-            filterFiles = dirContents: lib.attrsets.filterAttrs (_: fileType: elem fileType ["regular" "symlink"]) dirContents;
           in (
             if (pathExists "${appsPath}/${pkg.pname}.desktop")
             then "${appsPath}/${pkg.pname}.desktop"
-            else
-              (
-                if pathExists "${appsPath}"
-                then "${appsPath}/${head (attrNames (filterFiles (readDir "${appsPath}")))}"
-                else throw "no desktop file for app ${pkg.pname}"
-              )
+            else throw "no desktop file for app ${pkg.pname}"
           );
         };
     })
@@ -33,7 +29,6 @@
 in {
   # https://nix-community.github.io/home-manager/options.xhtml
   imports = [
-    ./programs/git.nix
     ./programs/zed-editor.nix
     ./programs/zsh.nix
   ];
@@ -58,6 +53,22 @@ in {
         {package = pkgs.gnomeExtensions.undecorate;}
       ];
     };
+    git = {
+      enable = true;
+      aliases = {
+        c = "commit -m";
+        co = "checkout";
+        chp = "cherry-pick";
+        a = "add";
+        undo-commit = "reset HEAD~";
+        unstage = "reset HEAD --";
+      };
+      userName = "WillsterJohnson";
+      userEmail = "willster@willsterjohnson.com";
+      extraConfig = {
+        init.defaultBranch = "trunk";
+      };
+    };
     lsd = {
       enable = true;
       enableAliases = true;
@@ -71,10 +82,146 @@ in {
       enable = true;
       addKeysToAgent = "yes";
     };
+    zed-editor = {
+      enable = true;
+      extensions = [
+        "catppuccin"
+        "csv"
+        "deno"
+        "emmet"
+        "html"
+        "nix"
+        "rainbow-csv"
+        "scheme"
+        "scss"
+        "svelte"
+        "toml"
+      ];
+      userSettings = {
+        assistant = {
+          default_model = {
+            provider = "copilot_chat";
+            model = "gpt-4o";
+          };
+          version = "2";
+        };
+        autosave = "on_focus_change";
+        buffer_font_family = "Victor Mono";
+        buffer_font_size = 16;
+        buffer_font_weight = 500;
+        code_actions_on_format = {
+          "source.organizeImports" = true;
+        };
+        copy_on_select = true;
+        current_line_highlight = "gutter";
+        experimental.theme_overrides = {
+          syntax = {
+            # https://gist.github.com/WillsterJohnson/13d6e60f59842188cd756c839a93acd0
+            # https://github.com/catppuccin/zed/blob/main/zed.tera
+            constant = {
+              color = "#F38BA8";
+            };
+          };
+        };
+        formatter = "language_server";
+        file_scan_exclusions = [
+          "**/.git"
+          "**/.svn"
+          "**/.hg"
+          "**/CVS"
+          "**/.DS_Store"
+          "**/Thumbs.db"
+          "**/.classpath"
+          "**/.settings"
+          "**/node_modules"
+          "**/.turbo"
+        ];
+        format_on_save = "on";
+        hard_tabs = true;
+        inlay_hints = {
+          enabled = true;
+          show_type_hints = false;
+          show_parameter_hints = false;
+          show_other_hints = false;
+        };
+        languages = {
+          TypeScript = {
+            language_servers = ["deno" "!typescript-language-server" "!vtsls" "!eslint"];
+          };
+          TSX = {
+            language_servers = ["deno" "!typescript-language-server" "!vtsls" "!eslint"];
+          };
+        };
+        lsp = {
+          deno = {
+            settings = {
+              deno = {
+                enable = true;
+              };
+            };
+          };
+        };
+        preferred_line_length = 120;
+        show_whitespaces = "boundary";
+        tab_size = 4;
+        tabs = {
+          git_status = true;
+        };
+        terminal = {
+          shell = {
+            program = "zsh";
+          };
+        };
+        theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "Catppuccin Mocha";
+        };
+        ui_font_size = 16;
+        ui_font_family = "Victor Mono";
+        ui_font_weight = 500;
+        vertical_scroll_margin = 100;
+        wrap_guides = [80 100 120];
+      };
+    };
     zoxide = {
       enable = true;
       enableZshIntegration = true;
       options = ["--cmd cd"];
+    };
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      autosuggestion.strategy = ["completion"];
+      envExtra = ''
+        export PATH="/home/willsterjohnson/.deno/bin:$PATH"
+        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
+      '';
+      initExtra = builtins.readFile ./zshrc;
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
+      shellAliases = {
+        b = "bun";
+        c = "clear";
+        cat = "bat";
+        ll = lib.mkForce "ls -lA --group-directories-first --git";
+        nixup = "/etc/nixos/nixup";
+        nixedit = "z /etc/nixos";
+        plz = "sudo";
+        z = "zeditor";
+        list-firefox-addons = "nix flake show gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      };
+      syntaxHighlighting.enable = true;
     };
   };
   home = {
@@ -85,7 +232,8 @@ in {
       EDITOR = "nano";
     };
     file =
-      {
+      autostartFile
+      // {
         files = {
           recursive = true;
           target = ".files";
@@ -101,8 +249,7 @@ in {
           target = ".fonts";
           source = ./.fonts;
         };
-      }
-      // autostartFile;
+      };
   };
   programs.home-manager.enable = true;
 }
