6e18378e27138a60f9686386a0f98a76e6cdca66
WillsterJohnson <willster@willsterjohnson.com>
2024-12-10 10:22:34 +0000

    259 current  2024-12-10 10:21:01  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/configuration.nix
+++ hosts/willsterjohnson/configuration.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: {
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = ["user-with-access-to-virtualbox"];
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
