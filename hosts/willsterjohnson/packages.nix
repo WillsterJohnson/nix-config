a268577d976067129ec600790a0d95b73cedb64f
WillsterJohnson <willster@willsterjohnson.com>
2025-01-20 15:55:28 +0000

    277 current  2025-01-20 15:55:26  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/packages.nix
+++ hosts/willsterjohnson/packages.nix
@@ -21,6 +21,7 @@
     nodejs_22 # required for 'zed-editor' due to temporary bug
     obsidian # note taking app
     openssl # why is this not included by default?
+    pkg-config # pkg-config
     protontricks # protontricks
     python310 # required for terminal extension in 'obsidian' (for some reason)
     rustup # rust toolchain
