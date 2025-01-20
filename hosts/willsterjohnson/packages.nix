0263736c18130468d24aa8d3de85def6a5bf0e5a
WillsterJohnson <willster@willsterjohnson.com>
2025-01-20 16:32:54 +0000

    282 current  2025-01-20 16:32:51  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/packages.nix
+++ hosts/willsterjohnson/packages.nix
@@ -15,7 +15,7 @@
     eclipses.eclipse-java # eclipse ide - the worst ide ever (but it supports old java so...)
     gcc # required for rust toolchain
     gitmoji-cli # better git commit
-    glibc # glibc
+    glib # glib
     gnome-tweaks # gnome tweaks
     jdk8 # java 8
     nixd # nix lsp support
