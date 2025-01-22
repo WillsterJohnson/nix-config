bc769d9cf17a0ae9a298d2f8ff70d78f2fff499a
WillsterJohnson <willster@willsterjohnson.com>
2025-01-22 22:08:56 +0000

    293 current  2025-01-22 22:08:53  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/packages.nix
+++ hosts/willsterjohnson/packages.nix
@@ -18,7 +18,7 @@
     gnome-tweaks # gnome tweaks
     inotify-tools # fs watch and more
     jetbrains.idea-community # intellij idea community edition
-    jdk8 # jdk8
+    jdk21 # jdk
     nixd # nix lsp support
     nodejs_22 # required for 'zed-editor' due to temporary bug
     obsidian # note taking app
