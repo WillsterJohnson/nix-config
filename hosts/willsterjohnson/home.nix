f3436c4e15def5d8cd0847bec21913d570d940c7
WillsterJohnson <willster@willsterjohnson.com>
2025-01-23 00:04:01 +0000

    298 current  2025-01-23 00:03:58  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/home.nix
+++ hosts/willsterjohnson/home.nix
@@ -204,7 +204,7 @@ in {
       autosuggestion.strategy = ["completion"];
       envExtra = ''
         export PATH="/home/willsterjohnson/.deno/bin:$PATH"
-        export PATH="/nix/store/xz2jwx2j70jd7zla1cxd0cmcshjpjwby-zulu-ca-jdk-8.0.402:$PATH"
+        export JAVA_HOME_JDK8="${pkgs.zulu8}"
         export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
       '';
       initExtra = builtins.readFile ./zshrc;
