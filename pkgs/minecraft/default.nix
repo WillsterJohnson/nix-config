cdf21afdcba56a56002097469603928c40ec79b2
WillsterJohnson <willster@willsterjohnson.com>
2025-01-31 13:42:17 +0000

    324 current  2025-01-31 01:26:06  24.11.20241026.86e78d3  6.6.58                          *

---
+++ pkgs/minecraft/default.nix
@@ -0,0 +1,190 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  nixosTests,
+  copyDesktopItems,
+  makeDesktopItem,
+  makeWrapper,
+  wrapGAppsHook3,
+  gobject-introspection,
+  zulu8,
+  xorg,
+  zlib,
+  nss,
+  nspr,
+  fontconfig,
+  pango,
+  cairo,
+  expat,
+  alsa-lib,
+  cups,
+  dbus,
+  atk,
+  gtk3-x11,
+  gtk2-x11,
+  gdk-pixbuf,
+  libGL,
+  glib,
+  curl,
+  freetype,
+  libpulseaudio,
+  libuuid,
+  systemd,
+  flite ? null,
+  libXxf86vm ? null,
+}: let
+  desktopItem = makeDesktopItem {
+    name = "minecraft";
+    exec = "minecraft";
+    icon = "minecraft";
+    comment = "Official launcher for Minecraft, a sandbox-building game";
+    desktopName = "Minecraft";
+    categories = ["Game"];
+  };
+
+  envLibPath = lib.makeLibraryPath [
+    curl
+    libpulseaudio
+    systemd
+    alsa-lib # needed for narrator
+    flite # needed for narrator
+    libGL
+    xorg.libX11
+    xorg.libxcb
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXScrnSaver
+  ];
+
+  libPath = lib.makeLibraryPath ([
+      # grep launcher_log.txt for 'java.lang.UnsatisfiedLinkError'
+      alsa-lib
+      atk
+      cairo
+      cups
+      dbus
+      expat
+      fontconfig
+      freetype
+      gdk-pixbuf
+      glib
+      gtk3-x11
+      gtk2-x11
+      libuuid
+      nspr
+      nss
+      pango
+      stdenv.cc.cc
+      zlib
+    ]
+    ++ (with xorg; [
+      libX11
+      libxcb
+      libXcomposite
+      libXcursor
+      libXdamage
+      libXext
+      libXfixes
+      libXi
+      libXrandr
+      libXrender
+      libXtst
+      libXScrnSaver
+    ]));
+in
+  stdenv.mkDerivation rec {
+    # fetch the url "https://launchermeta.mojang.com/v1/products/launcher/6f083b80d5e6fabbc4236f81d0d8f8a350c665a9/linux.json"
+    # it returns json string, parse it
+    # read the item at `."launcher-core"[0].version.name`, this is the value for the version
+    version =
+      builtins.elemAt
+      (
+        builtins.fromJSON
+        (
+          fetchurl {
+            url = "https://launchermeta.mojang.com/v1/products/launcher/6f083b80d5e6fabbc4236f81d0d8f8a350c665a9/linux.json";
+          }
+        )
+        .launcher-core
+        0
+      )
+      .version
+      .name;
+
+    pname = builtins.trace "${version}" "minecraft";
+
+    fubar = "not allowed";
+
+    src = fetchurl {
+      url = "https://launcher.mojang.com/download/linux/x86_64/minecraft-launcher_${version}.tar.gz";
+      sha256 = "03q579hvxnsh7d00j6lmfh53rixdpf33xb5zlz7659pvb9j5w0cm";
+    };
+
+    icon = fetchurl {
+      url = "https://launcher.mojang.com/download/minecraft-launcher.svg";
+      sha256 = "0w8z21ml79kblv20wh5lz037g130pxkgs8ll9s3bi94zn2pbrhim";
+    };
+
+    nativeBuildInputs = [makeWrapper wrapGAppsHook3 copyDesktopItems gobject-introspection];
+
+    sourceRoot = ".";
+
+    dontWrapGApps = true;
+    dontConfigure = true;
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/opt
+      mv minecraft-launcher $out/opt/
+      mv $out/opt/minecraft-launcher $out/opt/minecraft
+      mv $out/opt/minecraft/minecraft-launcher $out/opt/minecraft/minecraft
+
+      install -D $icon $out/share/icons/hicolor/symbolic/apps/minecraft.svg
+
+      runHook postInstall
+    '';
+
+    preFixup = ''
+      patchelf \
+        --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+        --set-rpath '$ORIGIN/'":${libPath}" \
+        $out/opt/minecraft/minecraft
+      patchelf \
+        --set-rpath '$ORIGIN/'":${libPath}" \
+        $out/opt/minecraft/libcef.so
+      patchelf \
+        --set-rpath '$ORIGIN/'":${libPath}" \
+        $out/opt/minecraft/liblauncher.so
+    '';
+
+    postFixup = ''
+      # Do not create `GPUCache` in current directory
+      makeWrapper $out/opt/minecraft/minecraft $out/bin/minecraft \
+        --prefix LD_LIBRARY_PATH : ${envLibPath} \
+        --prefix PATH : ${lib.makeBinPath [zulu8]} \
+        --set JAVA_HOME ${lib.getBin zulu8} \
+        --chdir /tmp \
+        "''${gappsWrapperArgs[@]}"
+    '';
+
+    desktopItems = [desktopItem];
+
+    meta = with lib; {
+      description = "Official launcher for Minecraft, a sandbox-building game";
+      homepage = "https://minecraft.net";
+      maintainers = with maintainers; [willsterjohnson];
+      sourceProvenance = with sourceTypes; [binaryNativeCode];
+      license = licenses.unfree;
+      platforms = ["x86_64-linux"];
+    };
+  }
