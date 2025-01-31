26d69dcaaea679be85870c3d41a194652c4a5d71
WillsterJohnson <willster@willsterjohnson.com>
2025-01-31 14:07:25 +0000

    326 current  2025-01-31 14:07:24  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/packages.nix
+++ hosts/willsterjohnson/packages.nix
@@ -33,7 +33,7 @@
     zulu17 # zulu17
     inputs.zen-browser.packages.x86_64-linux.specific # zen browser
 
-    # (pkgs.callPackage ../../pkgs/minecraft.nix {})
+    (pkgs.callPackage ../../pkgs/minecraft.nix {})
   ];
   environment.variables = {
     JAVA_HOME_JDK17 = "${pkgs.zulu17}";
