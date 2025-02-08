848b368f4144f6096ad750e7fee1caf0dc0b3c49
WillsterJohnson <willster@willsterjohnson.com>
2025-02-08 01:07:38 +0000

    359 current  2025-02-08 01:07:36  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/packages.nix
+++ hosts/willsterjohnson/packages.nix
@@ -31,7 +31,7 @@
     inputs.zen-browser.packages.x86_64-linux.specific # zen browser
   ];
   environment.variables = {
-    JAVA_HOME = "${pkgs.jdk21}";
+    JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
   };
   fonts.packages = with pkgs; [
     victor-mono
