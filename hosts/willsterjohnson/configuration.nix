4b7a49c4b85c0d8729c37cd86bea93c0b9f66965
WillsterJohnson <willster@willsterjohnson.com>
2025-03-26 01:19:23 +0000

    368 current  2025-03-24 23:37:55  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/configuration.nix
+++ hosts/willsterjohnson/configuration.nix
@@ -50,7 +50,10 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber = {
-      extraConfig = {
+      willsters-rage-at-handsfree-knows-no-rational-bounds = {
+        "monitor.bluez.properties" = {
+          "override.bluez5.roles" = ["a2dp_sink" "a2dp_source" "bap_sink" "bap_source" "hsp_hs" "hsp_ag" "hfp_ag"];
+        };
         "override.monitor.bluez.properties" = {
           "bluez5.enable-msbc" = false;
           "bluez5.enable-soc" = false;
