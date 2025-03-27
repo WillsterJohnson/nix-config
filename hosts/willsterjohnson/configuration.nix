17f3130d4cba7e4aaf9a00c59fb38578897e4dc3
WillsterJohnson <willster@willsterjohnson.com>
2025-03-27 17:48:55 +0000

    370 current  2025-03-26 01:24:32  24.11.20241026.86e78d3  6.6.58                          *

--- hosts/willsterjohnson/configuration.nix
+++ hosts/willsterjohnson/configuration.nix
@@ -44,7 +44,7 @@
   services.printing.enable = true;
   services.pipewire = {
     enable = true;
-    # audio.enable = true;
+    audio.enable = true;
     alsa.enable = true;
     # alsa.support32Bit = true;
     pulse.enable = true;
